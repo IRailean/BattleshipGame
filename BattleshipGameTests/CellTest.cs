15c323eea04f9ecfd6ac61d391b21234fb0e5f05
IRailean <armyclub@rambler.ru>
2021-04-09 18:39:17 +0300

    Add xUnitTest project

---
+++ BattleshipGameTests/CellTest.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using BattleshipGame.UIComponents;
+using BattleshipGame.Ships;
+using FluentAssertions;
+using BattleshipGame.Enums;
+
+namespace BattleshipGameTests
+{
+    public class CellTest
+    {
+        [Fact]
+        public void SetShip_ValidShip()
+        {
+            //Arrange
+            var expectedState = State.HasShipNotChecked;
+
+            //Action
+            var cell = new Cell();
+            var ship = new Battleship();
+
+            cell.SetShip(ship);
+
+            //Assert
+            cell.State.Should().Be(expectedState);
+            cell.Ship.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void SetShip_NullShip()
+        {
+            //Arrange
+            var expectedState = State.EmptyNotChecked;
+
+            //Action
+            var cell = new Cell();
+
+            cell.SetShip(null);
+
+            //Assert
+            cell.State.Should().Be(expectedState);
+            cell.Ship.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData(State.EmptyChecked, State.EmptyChecked)]
+        [InlineData(State.EmptyNotChecked, State.EmptyNotChecked)]
+        [InlineData(State.HasShipChecked, State.HasShipChecked)]
+        [InlineData(State.HasShipNotChecked, State.HasShipNotChecked)]
+        [InlineData(State.Default, State.EmptyNotChecked)]
+        public void ChangeStateTest(State setState, State expectedState)
+        {
+            //Arrange
+
+            //Action
+            var cell = new Cell();
+            cell.ChangeState(setState);
+
+            //Assert
+            cell.State.Should().Be(expectedState);
+        }
+    }
+}
---
+++ BattleshipGameTests/GridTest.cs
@@ -0,0 +1,75 @@
+﻿using BattleshipGame;
+using BattleshipGame.Enums;
+using BattleshipGame.Ships;
+using BattleshipGame.UIComponents;
+using FluentAssertions;
+using Xunit;
+
+namespace BattleshipGameTests
+{
+    public class GridTest
+    {
+        [Theory]
+        [InlineData(10, 0, 0)]
+        public void GetShipAt_ValidCoordinates_ReturnedShip(int size, int x, int y)
+        {
+            //Arrange
+            var coordinates = new Coordinates { X = x, Y = y };
+            var grid = new Grid(size);
+            grid.GetCell(coordinates).SetShip(new Battleship());
+
+            //Action
+            var ship = grid.GetShipAt(coordinates);
+
+            //Assert
+            ship.Name.Should().Be("Battleship");
+        }
+
+        [Theory]
+        [InlineData(10, -1, -1)]
+        [InlineData(10, 11, 11)]
+        public void GetCell_InvalidCoordinates_ReturnsNull(int size, int x, int y)
+        {
+            //Arrange
+            var coordinates = new Coordinates { X = x, Y = y };
+            var grid = new Grid(size);
+            grid.GetCell(coordinates);
+
+            //Action
+            var cell = grid.GetCell(coordinates);
+
+            //Assert
+            cell.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetCellState_ValidCoordinates_ReturnStateEmptyNotChecked()
+        {
+            //Arrange
+            var coordinates = new Coordinates { X = 0, Y = 0 };
+            var grid = new Grid(10);
+
+            //Action
+            var state = grid.GetCellState(coordinates);
+
+            //Assert
+            state.Should().Be(State.EmptyNotChecked);
+        }
+
+        [Fact]
+        public void GetCellState_InvalidCoordinates_ReturnStateDefault()
+        {
+            //Arrange
+            var coordinates = new Coordinates { X = -1, Y = -1 };
+            var grid = new Grid(10);
+
+            //Action
+            var state = grid.GetCellState(coordinates);
+
+            //Assert
+            state.Should().Be(State.Default);
+        }
+
+
+    }
+}
